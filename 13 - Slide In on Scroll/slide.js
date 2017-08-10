c04f287e8a3ee3cf62c7a1e3f2cf7222b40d0cf7
Joshua Scott <jscott313@gmail.com>
2017-08-10 14:00:21 +0300

    Completed day 13

--- 13 - Slide In on Scroll/slide.js
+++ 13 - Slide In on Scroll/slide.js
@@ -14,3 +14,24 @@ function debounce(func, wait = 20, immediate = true) {
     };
 }
 
+const slideImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    slideImages.forEach(slideImage => {
+        const bottomPixels = window.scrollY + window.innerHeight;       // Pixel count at the very bottom of screen
+        const slideInAt = bottomPixels - (slideImage.height / 2);       // When exactly half of the image is peeking from the bottom of screen
+        const imageBottom = slideImage.offsetTop + slideImage.height;   // Pixel count of the bottom of the image (offsetTop = pixels from top of screen to top of image)
+
+        const isHalfShown = slideInAt > slideImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;         // Slide image back out when it's offscreen
+
+        // If true, the image must be in the 'sweet spot' where it's at least half-visible from the bottom, and not somewhere above the screen
+        if (isHalfShown && isNotScrolledPast) {
+            slideImage.classList.add('active');
+        } else {
+            slideImage.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
