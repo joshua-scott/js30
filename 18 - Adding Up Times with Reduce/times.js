2014eb8ae4d74b4d41d19d23e14fb40cbba535d5
Joshua Scott <jscott313@gmail.com>
2017-08-22 16:52:03 +0300

    Completed day 18

---
+++ 18 - Adding Up Times with Reduce/times.js
@@ -0,0 +1,25 @@
+// We must convert the nodeList into an array.
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+
+let seconds = timeNodes
+    // time as string
+    .map(node => node.dataset.time)
+    // es6 destructuring syntax to get mins and secs, then convert to secs only
+    .map(timeCode => {
+        const [mins, secs] = timeCode.split(':').map(parseFloat);
+        return (mins * 60) + secs;
+    })
+    // reduce to one total number of seconds
+    .reduce((a, b) => a + b);
+
+// convert seconds into hours/mins/secs
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+// log the result
+console.log(hours, mins, secondsLeft);
