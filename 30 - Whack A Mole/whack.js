184ddf4a8996a42614b91b8fa22fad985bf2c23e
Joshua Scott <jscott313@gmail.com>
2017-09-08 19:22:26 +0300

    Completed day 30

--- 30 - Whack A Mole/whack.js
+++ 30 - Whack A Mole/whack.js
@@ -1,3 +1,46 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomTime(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function randomHole(holes) {
+  const hole = holes[Math.floor(Math.random() * holes.length)];
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const hole = randomHole(holes);
+  const time = randomTime(200, 1000);
+  hole.classList.add('up');
+  hole.addEventListener('click', bonk);
+  setTimeout(() => {
+    hole.removeEventListener('click', bonk);
+    hole.classList.remove('up');
+    if (!timeUp) peep();
+  }, time);
+}
+
+function bonk() {
+  this.removeEventListener('click', bonk);
+  this.classList.remove('up');
+  scoreBoard.textContent = ++score;
+}
+
+function startGame() {
+  score = 0;
+  scoreBoard.textContent = score;
+  timeUp = false;
+  peep();
+  setTimeout(() => timeUp = true, 10000);
+}
+
+// startGame();
